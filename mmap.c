66dd89cb280fcd4cfeaa2ac10c21ca56dc974f99
Kevin Benavides <126353237+DevCheckOG@users.noreply.github.com>
2025-08-10 11:50:07 -0600

    Update mmap.c

--- mmap.c
+++ mmap.c
@@ -2,13 +2,6 @@
 #include <stdint.h>
 #include <errno.h>
 
-#define SYS_MMAP 9
-#define SYS_MUNMAP 11
-#define PROT_READ 0x1
-#define PROT_WRITE 0x2
-#define MAP_PRIVATE 0x2
-#define MAP_ANONYMOUS 0x20
-
 void* mmap(void* addr, size_t length, int prot, int flags, int fd, off_t offset) {
     void* result;
 
